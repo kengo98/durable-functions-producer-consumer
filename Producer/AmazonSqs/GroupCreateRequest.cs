50929ef0a568e4afb206745f79f2385a720aaa09
Brandon H <hurlburb@microsoft.com>
2019-06-26 14:13:12 -0700

    Posting to amazon SQS working

---
+++ Producer/AmazonSqs/GroupCreateRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Producer.AmazonSqs
+{
+    internal class GroupCreateRequest
+    {
+        public string GroupId { get; set; }
+        public int NumberOfMessagesPerGroup { get; set; }
+        public string TestRunId { get; set; }
+    }
+}
---
+++ Producer/AmazonSqs/GroupMessagesCreateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Producer.AmazonSqs
+{
+    internal class GroupMessagesCreateRequest
+    {
+        public string GroupId { get; set; }
+        public int MessageId { get; set; }
+        public DateTime EnqueueTimeUtc { get; set; }
+        public string TestRunId { get; set; }
+    }
+}
